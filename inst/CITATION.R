f628a83e7edbbd1bdaa4bda58e337105dd00ce22
Benjamin Delmas <del.benjamin75@gmail.com>
2025-07-28 16:37:51 +0200

    ajout license en R

---
+++ inst/CITATION.R
@@ -0,0 +1,31 @@
+citHeader("To cite the FrailtyCompRisk package in publications use:")
+
+citEntry(
+  entry = "Manual",
+  title = "FrailtyCompRisk: Estimation Methods for Competing Risks Models with Shared Frailty",
+  author = personList(
+    person("Sandrine", "Katsahian", role = c("aut", "cre"), comment = c(ORCID = "0000-0002-7261-0671")),
+    person("Lucas", "Ducrot", role = "aut", comment = c(ORCID = "0000-0002-8020-2897")),
+    person("Benjamin", "Delmas", role = "aut", email = "benjamin.delmas@ensae.fr")
+  ),
+  year = "2025",
+  note = "R package version 0.1.0",
+  url = "https://github.com/TeamHeKA/FrailtyCompRisk"
+)
+
+citEntry(
+  entry = "Article",
+  title = "Analysing multicentre competing risks data with a mixed proportional hazards model for the subdistribution",
+  author = personList(
+    person("Sandrine", "Katsahian"),
+    person("Matthieu", "Resche-Rigon"),
+    person("Sylvie", "Chevret"),
+    person("Raphaël", "Porcher")
+  ),
+  journal = "Statistics in Medicine",
+  year = "2006",
+  volume = "25",
+  number = "24",
+  pages = "4267–4278",
+  doi = "10.1002/sim.2684"
+)
